5a297e46b67f56e5429b121f75a54866dc3aa3a7
DoDuy2004 <duy2004.gi@gmail.com>
2025-02-09 20:12:36 +0700

    first commit

---
+++ GundamStore/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using GundamStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GundamStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var breadcrumbs = new List<BreadcrumbItem>
+            {
+                new BreadcrumbItem { Title = "Trang chủ", Url = Url.Action("Index", "Home") ?? "/", IsActive = false },
+                new BreadcrumbItem { Title = "Tất cả sản phẩm", Url = "#", IsActive = true }
+            };
+
+            ViewData["Breadcrumbs"] = breadcrumbs;
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            var breadcrumbs = new List<BreadcrumbItem>
+            {
+                new BreadcrumbItem { Title = "Trang chủ", Url = Url.Action("Index", "Home") ?? "/", IsActive = false },
+                new BreadcrumbItem { Title = "Tên danh mục cha", Url = Url.Action("Index", "Product", new { CategoryId = 1 }) ?? "/", IsActive = false },
+                new BreadcrumbItem { Title = "Tên sản phẩm", Url = "#", IsActive = true }
+            };
+
+            ViewData["Breadcrumbs"] = breadcrumbs;
+            return View();
+        }
+    }
+}
---
+++ GundamStore/Models/BreadcrumbItem.cs
@@ -0,0 +1,9 @@
+﻿namespace GundamStore.Models
+{
+    public class BreadcrumbItem
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+    }
+}
